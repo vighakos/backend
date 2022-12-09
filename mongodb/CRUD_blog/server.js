1568dbebd2d60bb9880caab86346600589de1e59
vighakos <vighakos@turr.hu>
2022-12-09 10:09:42 +0100

    mongo

--- mongodb/CRUD_blog/server.js
+++ mongodb/CRUD_blog/server.js
@@ -1,46 +1,92 @@
-const { application } = require('express');
-const moment = require('moment');
-
-const express = require('express'),
+require('dotenv').config()
+const { application } = require('express'),
+    moment = require('moment'),
+    cors = require('cors'),
+    token = process.env.TOKEN,
+    express = require('express'),
     { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'),
     server = express(),
     port = 5000
 
+server.use(cors())
 server.use(express.urlencoded({extended:true}))
+server.use(express.json())
 
-const uri = "mongodb+srv://vighakos:asd123@xd.g2xsjai.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@${process.env.DBHOST}/?retryWrites=true&w=majority`,
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 client.connect(err => {
     console.log('Connected to database');
-    const blogs = client.db("CRUD_blog").collection("blogs");
+    const database = client.db(process.env.DBNAME);
 
-    server.get('/blogs', (req, res) => {
-        blogs.find().toArray()
-            .then(results => {
-                res.send(results)
-            })
+    server.post('/login', tokencheck(), (req, res) => {
+        var table = req.body.table;
+        var email = req.body.email;
+        var passwd = req.body.password;
+        let collection = database.collection(table)
+    
+
+        collection.find({$and:[{ "email": email, "passwd": passwd }]}).toArray()
+            .then(results => { res.send(results) })
+            .catch(err => { console.log(err); })
+    });
+
+    server.get('/:table', tokencheck(), (req, res) => {
+        let table = req.params.table
+        let collection = database.collection(table)
+
+        collection.find().toArray()
+            .then(results => { res.send(results) })
             .catch(err => { console.log(err); })
     })
     
-    server.get('/blogs/:id', (req, res) => {
-        let blogID = req.params.id
-        blogs.find({"_id":ObjectId(blogID)}).toArray()
-            .then(results => {
-                res.send(results)
-            })
+    server.get('/:table/:id', tokencheck(), (req, res) => {
+        let table = req.params.table
+        let collection = database.collection(table)
+        let ID = req.params.id
+
+        collection.find({"_id":ObjectId(ID)}).toArray()
+            .then(results => { res.send(results) })
             .catch(err => { console.log(err); })
     })
 
-    server.post('/blogs', (req, res) => {
-        let data = {
-            title: req.body.title,
-            description: req.body.description,
-            date: moment(new Date()).format('YYY-MM-DD H:m:s')
-        }
-        blogs.insertOne(data)
-            .then(results => {
-                res.send(results)
-            })
+    server.post('/:table', tokencheck(), (req, res) => {
+        let table = req.params.table
+        let collection = database.collection(table)
+        let data = req.body
+
+        collection.insertOne(data)
+            .then(results => { res.send(results) })
+            .catch(err => { console.log(err); })
+    })
+
+    server.patch('/:table/:id', tokencheck(), (req, res) => {
+        let table = req.params.table
+        let collection = database.collection(table)
+        let ID = req.params.id
+        let data = req.body
+
+        collection.updateOne( {"_id":ObjectId(ID)}, {$set: data})
+            .then(results => { res.send(results) })
+            .catch(err => { console.log(err); })
+    })
+
+    server.delete('/:table', tokencheck(), (req, res) => {
+        let table = req.params.table
+        let collection = database.collection(table)
+
+        collection.deleteMany({})
+            .then(results => { res.send(results) })
+            .catch(err => { console.log(err); })
+    })
+
+    server.get('/:table/:id', tokencheck(), (req, res) => {
+        let table = req.params.table
+        let collection = database.collection(table)
+        let ID = req.params.id
+
+        collection.deleteOne({ "_id": ObjectId(ID) })
+            .then(results => { res.send(results) })
             .catch(err => { console.log(err); })
     })
 
@@ -48,3 +94,13 @@ client.connect(err => {
 });
 
 server.listen(port, () => { console.log(`Server listening on ${port}`) })
+
+function tokencheck() {
+    return (req, res, next) => {
+        if (req.headers.authorization == token) {
+            next();
+        } else {
+            res.status(500).json({ message: 'Illetéktelen hozzáférés!' });
+        }
+    };
+}
