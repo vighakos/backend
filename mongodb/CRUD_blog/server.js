6ddc508ac7bbb47ff8eaba572436886a7674fea7
vighakos <vighakos@turr.hu>
2022-12-15 12:30:01 +0100

    mongo

--- mongodb/CRUD_blog/server.js
+++ mongodb/CRUD_blog/server.js
@@ -1,112 +1,249 @@
-require('dotenv').config()
-const { application } = require('express'),
-    moment = require('moment'),
-    cors = require('cors'),
-    token = process.env.TOKEN,
-    express = require('express'),
-    ejs = require('ejs'),
-    { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'),
-    server = express(),
-    port = 5000
+require('dotenv').config();
+const express = require('express');
+const moment = require('moment');
+const path = require('path');
+const multer = require('multer');
+const cors = require('cors');
+const ejs = require('ejs');
+const axios = require('axios');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = 5000;
+const token = process.env.TOKEN;
+const version = process.env.VERSION;
+const debug = process.env.DEBUG;
 
-server.use(cors())
-server.use(express.urlencoded({extended:true}))
-server.use(express.json())
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@${process.env.DBHOST}/?retryWrites=true&w=majority`,
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+var pug = require('pug');
+var fn = pug.compile('string of pug', options);
+var html = fn(locals);
 
+// Image file Upload settings
+var storage = multer.diskStorage({
+    destination: '../Public/uploads/',
+    filename: function(req, file, cb) {
+        let file_name = file.originalname.replace(path.extname(file.originalname), "") + '-' + Date.now() + path.extname(file.originalname);
+        cb(null, file_name);
+    }
+});
+
+var upload = multer({ storage: storage });
+
+const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@${process.env.DBHOST}/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-    console.log('Connected to database');
+    console.log('Connected to MongoDB Database...');
     const database = client.db(process.env.DBNAME);
 
-    server.post('/login', tokencheck(), (req, res) => {
+    app.get('/', (req, res) => {
+
+        axios.get('http://localhost:5000/blogs').then(function(results) {
+            console.log(results)
+            ejs.renderFile('views/blogs.ejs', { blogs: results.data }, (err, data) => {
+                res.send(data);
+            })
+        });
+
+    });
+
+
+
+
+    // file upload
+    app.post('/fileUpload', upload.single('file'), (req, res) => {
+        log(req.socket.remoteAddress, `1 File uploaded to /Public/uploads (${req.file.filename}`);
+        res.status(200).json(req.file);
+    });
+
+    // GET VERSION INFO
+    app.get('/', (req, res) => {
+        log(req.socket.remoteAddress, `Sent version information.`);
+        res.status(200).send(`2/14.szft Backend MongoDB API ${version}.`);
+    });
+
+    // LOGINCHECK
+    app.post('/login', tokencheck(), (req, res) => {
         var table = req.body.table;
         var email = req.body.email;
         var passwd = req.body.password;
-        let collection = database.collection(table)
-    
+        let collection = database.collection(table);
 
-        collection.find({$and:[{ "email": email, "passwd": passwd }]}).toArray()
-            .then(results => { res.send(results) })
-            .catch(err => { console.log(err); })
+        //TODO: tesztelni kell!
+        collection.find({ $and: [{ "email": email }, { 'passwd': passwd }] }).toArray()
+            .then(results => {
+                log(req.socket.remoteAddress, `${results.length} records sent form ${table} table (logincheck).`);
+                res.send(results);
+            })
+            .catch(error => {
+                log(req.socket.remoteAddress, error);
+                res.send(error);
+            });
     });
 
-    server.get('/:table', (req, res) => {
-        let table = req.params.table
-        let collection = database.collection(table)
-
+    // GET ALL RECORDS
+    app.get('/:table', tokencheck(), (req, res) => {
+        let table = req.params.table;
+        let collection = database.collection(table);
         collection.find().toArray()
-            .then(results => { 
-                ejs.renderFile('public/index.ejs', {results}, (err, data) => {
-                    if (err) res.send(err)
-                    res.send(data)
-                })
-             })
-            .catch(err => { console.log(err); })
-    })
-    
-    server.get('/:table/:id', tokencheck(), (req, res) => {
-        let table = req.params.table
-        let collection = database.collection(table)
-        let ID = req.params.id
+            .then(results => {
+                log(req.socket.remoteAddress, `${results.length} records sent form ${table} table.`);
+                res.send(results);
+            })
+            .catch(error => {
+                log(req.socket.remoteAddress, error);
+                res.send(error);
+            });
+    });
 
-        collection.find({"_id":ObjectId(ID)}).toArray()
-            .then(results => { res.send(results) })
-            .catch(err => { console.log(err); })
-    })
+    // GET ONE RECORD BY ID
+    app.get('/:table/:id', tokencheck(), (req, res) => {
+        let ID = req.params.id;
+        let table = req.params.table;
+        let collection = database.collection(table);
+        collection.find({ "_id": ObjectId(ID) }).toArray()
+            .then(results => {
+                log(req.socket.remoteAddress, `${results.length} record sent form ${table} table.`);
+                res.send(results);
+            })
+            .catch(error => {
+                log(req.socket.remoteAddress, error);
+                res.send(error);
+            });
+    });
 
-    server.post('/:table', tokencheck(), (req, res) => {
-        let table = req.params.table
-        let collection = database.collection(table)
-        let data = req.body
+    // GET RECORDS BY field
+    app.get('/:table/:field/:value', tokencheck(), (req, res) => {
+        var table = req.params.table;
+        var field = req.params.field;
+        var value = req.params.value;
+        let collection = database.collection(table);
+        let felt = `{
+            "${field}":"${value}"
+        }`;
+        collection.find(JSON.parse(felt)).toArray()
+            .then(results => {
+                log(req.socket.remoteAddress, `${results.length} record sent form ${table} table.`);
+                res.send(results);
+            })
+            .catch(error => {
+                log(req.socket.remoteAddress, error);
+                res.send(error);
+            });
+    });
 
+    // INSERT RECORD
+    app.post('/:table', tokencheck(), (req, res) => {
+        let table = req.params.table;
+        let collection = database.collection(table);
+        let data = req.body;
         collection.insertOne(data)
-            .then(results => { res.send(results) })
-            .catch(err => { console.log(err); })
-    })
+            .then(results => {
+                log(req.socket.remoteAddress, `1 record inserted to ${table} table.`);
+                res.send(results)
+            })
+            .catch(error => {
+                log(req.socket.remoteAddress, error);
+                res.send(error);
+            });
+    });
 
-    server.patch('/:table/:id', tokencheck(), (req, res) => {
-        let table = req.params.table
-        let collection = database.collection(table)
-        let ID = req.params.id
-        let data = req.body
+    // UPDATE RECORD BY ID
+    app.patch('/:table/:id', tokencheck(), (req, res) => {
+        let table = req.params.table;
+        let collection = database.collection(table);
+        let blogID = req.params.id;
+        let data = req.body;
+        collection.updateOne({ "_id": ObjectId(blogID) }, { $set: data })
+            .then(results => {
+                log(req.socket.remoteAddress, `${results.modifiedCount} record updated in ${table} table.`);
+                res.send(results);
+            })
+            .catch(error => {
+                log(req.socket.remoteAddress, error);
+                res.send(error);
+            });
+    });
 
-        collection.updateOne( {"_id":ObjectId(ID)}, {$set: data})
-            .then(results => { res.send(results) })
-            .catch(err => { console.log(err); })
-    })
-
-    server.delete('/:table', tokencheck(), (req, res) => {
-        let table = req.params.table
-        let collection = database.collection(table)
-
+    // DELETE ALL RECORDS
+    app.delete('/:table', tokencheck(), (req, res) => {
+        let table = req.params.table;
+        let collection = database.collection(table);
         collection.deleteMany({})
-            .then(results => { res.send(results) })
-            .catch(err => { console.log(err); })
-    })
+            .then(results => {
+                log(req.socket.remoteAddress, `${results.deletedCount} record deleted form ${table} table.`);
+                res.send(results)
+            })
+            .catch(error => {
+                log(req.socket.remoteAddress, error);
+                res.send(error);
+            });
+    });
 
-    server.get('/:table/:id', tokencheck(), (req, res) => {
-        let table = req.params.table
-        let collection = database.collection(table)
-        let ID = req.params.id
-
+    // DELETE ONE RECORD BY ID
+    app.delete('/:table/:id', tokencheck(), (req, res) => {
+        let ID = req.params.id;
+        let table = req.params.table;
+        let collection = database.collection(table);
         collection.deleteOne({ "_id": ObjectId(ID) })
-            .then(results => { res.send(results) })
-            .catch(err => { console.log(err); })
-    })
+            .then(results => {
+                log(req.socket.remoteAddress, `${results.deletedCount} record deleted form ${table} table.`);
+                res.send(results)
+            })
+            .catch(error => {
+                log(req.socket.remoteAddress, error);
+                res.send(error);
+            });
+    });
 
-    //client.close();
+    // DELETE RECORDS BY field
+    app.delete('/:table/:field/:value', tokencheck(), (req, res) => {
+        var table = req.params.table;
+        var field = req.params.field;
+        var value = req.params.value;
+        let collection = database.collection(table);
+        let felt = `{
+                "${field}":"${value}"
+            }`;
+        collection.deleteMany(JSON.parse(felt)).toArray()
+            .then(results => {
+                log(req.socket.remoteAddress, `${results.length} record deleted form ${table} table.`);
+                res.send(results);
+            })
+            .catch(error => {
+                log(req.socket.remoteAddress, err);
+                res.send(error);
+            });
+    });
+
+    // client.close();
 });
 
-server.listen(port, () => { console.log(`Server listening on ${port}`) })
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+});
+
+// MIDDLEVARE FUNCTIONS
 
 function tokencheck() {
+
     return (req, res, next) => {
-        if (req.headers.authorization == token) {
-            next();
-        } else {
-            res.status(500).json({ message: 'Illetéktelen hozzáférés!' });
-        }
+        /*    if (req.headers.authorization == token) {
+              next();
+          } else {
+              res.status(500).json({ message: 'Illetéktelen hozzáférés!' });
+          }
+          */
+        next();
     };
+
+}
+
+function log(req, res) {
+    if (debug == 1) {
+        var timestamp = moment(new Date()).format('yyyy-MM-DD HH:mm:ss');
+        console.log(`[${timestamp}] : ${req} >>> ${res}`);
+    }
 }
